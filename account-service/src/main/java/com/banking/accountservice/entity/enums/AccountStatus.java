ddf1398cf11e7d9a6e7f6a2099d69cbf82fc0055
Mainak Chanda <mainakchanda531@gmail.com>
2026-09-13 19:32:29 +0530

    feat: initialize microservices architecture with Maven multi-module support and base configuration

---
+++ account-service/src/main/java/com/banking/accountservice/entity/enums/AccountStatus.java
@@ -0,0 +1,7 @@
+package com.banking.accountservice.entity.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    BLOCKED,
+    CLOSED
+}
---
+++ account-service/src/main/java/com/banking/accountservice/entity/enums/AccountType.java
@@ -0,0 +1,7 @@
+package com.banking.accountservice.entity.enums;
+
+public enum AccountType {
+    SAVING,
+    CURRENT,
+    FIXED_DEPOSIT
+}
